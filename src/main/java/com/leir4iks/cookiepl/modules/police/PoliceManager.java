bd7ce1e18e332d945d5c8d33b2abd94f2edef9d4
leir4iks <leir4iks@gmail.com>
2025-10-19 18:32:17 +0300

    fix bugs (5)

--- src/main/java/com/leir4iks/cookiepl/modules/police/PoliceManager.java
+++ src/main/java/com/leir4iks/cookiepl/modules/police/PoliceManager.java
@@ -139,7 +139,7 @@ public class PoliceManager {
         String command = plugin.getConfig().getString("modules.police-system.emote-commands." + type, "");
         if (command != null && !command.isEmpty()) {
             String finalCommand = command.replace("{player}", playerName);
-            plugin.getServer().getScheduler().runTask(plugin, () -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), finalCommand));
+            plugin.getFoliaLib().getGlobalRegionScheduler().run((task) -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), finalCommand));
         }
     }
 
@@ -180,7 +180,7 @@ public class PoliceManager {
                     if (location != null) {
                         plugin.getFoliaLib().getScheduler().runAtLocation(location, (task) -> uncuffPlayer(victimUUID));
                     } else {
-                        plugin.getServer().getScheduler().runTask(plugin, () -> uncuffPlayer(victimUUID));
+                        plugin.getFoliaLib().getGlobalRegionScheduler().run((task) -> uncuffPlayer(victimUUID));
                     }
                     logManager.debug("Auto-uncuffed player " + (victim != null ? victim.getName() : victimUUID) + " due to watchdog conditions.");
                 }
