033655faaed100213edec5fdf02bcf8f65855588
leir4iks <leir4iks@gmail.com>
2025-10-26 18:27:36 +0200

    fixinvulnerable

--- src/main/java/com/leir4iks/cookiepl/commands/MainCommand.java
+++ src/main/java/com/leir4iks/cookiepl/commands/MainCommand.java
@@ -2,9 +2,11 @@ package com.leir4iks.cookiepl.commands;
 
 import com.leir4iks.cookiepl.CookiePl;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class MainCommand implements CommandExecutor {
@@ -22,17 +24,37 @@ public class MainCommand implements CommandExecutor {
             return true;
         }
 
-        if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
-            if (sender.hasPermission("cookiepl.command.reload")) {
-                plugin.reloadAllModules();
-                sender.sendMessage(ChatColor.GREEN + "CookiePl configuration and modules reloaded.");
-            } else {
-                sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+        if (args.length > 0) {
+            if (args[0].equalsIgnoreCase("fixinvulnerable")) {
+                if (sender.hasPermission("cookiepl.command.admin")) {
+                    int fixedPlayers = 0;
+                    for (Player player : plugin.getServer().getOnlinePlayers()) {
+                        if (player.isInvulnerable() && player.getGameMode() != GameMode.CREATIVE && player.getGameMode() != GameMode.SPECTATOR) {
+                            player.setInvulnerable(false);
+                            fixedPlayers++;
+                        }
+                    }
+                    sender.sendMessage(ChatColor.GREEN + "Fixed " + fixedPlayers + " players stuck in invulnerability mode.");
+                    plugin.getLogManager().info("Admin " + sender.getName() + " ran invulnerability fix. Affected players: " + fixedPlayers);
+                } else {
+                    sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+                }
+                return true;
             }
-            return true;
+
+            if (args[0].equalsIgnoreCase("reload")) {
+                if (sender.hasPermission("cookiepl.command.reload")) {
+                    plugin.reloadAllModules();
+                    sender.sendMessage(ChatColor.GREEN + "CookiePl configuration and modules reloaded.");
+                } else {
+                    sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+                }
+                return true;
+            }
         }
 
         sender.sendMessage(ChatColor.AQUA + "This server is running CookiePl v" + plugin.getDescription().getVersion());
+        sender.sendMessage(ChatColor.GRAY + "Use /cookiepl <reload|fixinvulnerable>");
         return true;
     }
 }
